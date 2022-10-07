c093f6ecbe4fd9469f738c99cec7232a63261905
dyupina <55951388+dyupina@users.noreply.github.com>
2022-10-07 10:09:56 +0300

    2 commit

---
+++ file.cpp
@@ -0,0 +1,20 @@
+#include <haha.h>
+int function()
+{
+	
+	return 0;
+}
+
+int also_func()
+{
+	
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	function();
+
+	also_func();
+	return 0;
+}
